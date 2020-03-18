91ba6e9dbd88045d64ef56dc480a92d126fd2056
mindflayera <amitmishra19981@outlook.com>
2020-03-18 13:11:27 +0530

    First Commit

---
+++ src/main/java/com/zixcloudfoundary/ui/DynamoApplication.java
@@ -0,0 +1,26 @@
+package com.zixcloudfoundary.ui;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class DynamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder createObject() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
+}
